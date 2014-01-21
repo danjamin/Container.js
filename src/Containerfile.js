529d2228da66b18d2bd5eb4ceaccde4c6a9c2695
danjamin <e051ba3be@gmail.com>
2014-01-20 23:18:48 -0800

    [wip] Simplified the container file

--- src/Containerfile.js
+++ src/Containerfile.js
@@ -1,16 +1,6 @@
 // Containerfile.js
-(function(Containerfile, undefined) {
-    var env = 'dev';
-    var containerfile = {
-        document: document,
-        window: window,
-        env: env
-    };
-
-    if (env === 'dev') {
-        containerfile['dev'] = 5;
-    }
-
-    Containerfile(containerfile);
-
-})(Containerfile);
+window.Containerfile({
+    env: 'dev',
+    document: document,
+    window: window,
+});
